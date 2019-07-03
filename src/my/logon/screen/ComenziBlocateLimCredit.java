26c45e1330af60d3b7afb3594c300b90634e291a
Florin Brasoveanu <gflorinb@yahoo.com>
2019-07-03 12:17:24 +0300

    20190703

--- src/my/logon/screen/ComenziBlocateLimCredit.java
+++ src/my/logon/screen/ComenziBlocateLimCredit.java
@@ -176,16 +176,19 @@ public class ComenziBlocateLimCredit extends Activity implements ComenziDAOListe
 			if (UserInfo.getInstance().getTipAcces().equals("9"))
 				tipUser = "AV";
 
-			if (UserInfo.getInstance().getTipAcces().equals("10"))
+			else if (UserInfo.getInstance().getTipAcces().equals("10"))
 				tipUser = "SD";
 
-			if (UserInfo.getInstance().getTipAcces().equals("12") || UserInfo.getInstance().getTipAcces().equals("14"))
+			else if (UserInfo.getInstance().getTipAcces().equals("12") || UserInfo.getInstance().getTipAcces().equals("14"))
 				tipUser = "DV";
 
-			if (UserInfo.getInstance().getTipAcces().equals("27"))
+			else if (UserInfo.getInstance().getTipAcces().equals("27"))
 				tipUser = "KA";
+			
+			else if (UserInfo.getInstance().getTipAcces().equals("32") || UserInfo.getInstance().getTipUserSap().equals("SDKA"))
+				tipUser = "SK";				
 
-			if (UserInfo.getInstance().getTipAcces().equals("35"))
+			else if (UserInfo.getInstance().getTipAcces().equals("35"))
 				tipUser = "DK";
 
 			HashMap<String, String> params = new HashMap<String, String>();
