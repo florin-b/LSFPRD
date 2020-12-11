13cc3c3ce4d27f28a9fa1f8f3db405802192339b
Florin Brasoveanu <gflorinb@yahoo.com>
2020-12-11 11:37:42 +0200

    20201211

--- src/my/logon/screen/MainMenu.java
+++ src/my/logon/screen/MainMenu.java
@@ -72,7 +72,7 @@ public class MainMenu extends Activity {
 			R.drawable.location_icon, R.drawable.blank,
 			R.drawable.despre_icon, R.drawable.exit_icon, R.drawable.blank };
 
-	public String[] btnNamesSD = { "Utilizator", "Creare comanda", "Modificare comanda", "Afisare comanda", "Creare cmd GED",
+	public String[] btnNamesSD = { "Utilizator", "Creare comanda", "Modificare comanda", "Afisare comanda", "Creare cmd GED","Comenzi simulate",
 			"Cmz.blocate limita credit", "Creare CLP", "Aprobare CLP", "Afisare CLP",  "Aprobare DL", "Afisare DL", "Retur paleti",
 			"Stare retur paleti", "Retur comenzi", "Modificare custodie","Modificare data livrare", "Vanzari", "Neincasate", "Stocuri", "Obiective", "Preturi",
 			"Preturi concurenta", "Stare comenzi",  "Pierderi vanzari", "Salarizare", "Articole avarie", "Creare sablon", "Afisare sablon",
@@ -80,7 +80,7 @@ public class MainMenu extends Activity {
 			"Clienti alocati", "Adrese clienti",  "Delegatii", "Despre", "Iesire" };
 
 	public int[] btnImageSD = new int[] { R.drawable.id_icon, R.drawable.new_icon, R.drawable.modif_icon, R.drawable.preview_icon,
-			R.drawable.blue_basket_icon, R.drawable.cmd_bloc, R.drawable.clp, R.drawable.aprob_clp, R.drawable.afis_clp, 
+			R.drawable.blue_basket_icon, R.drawable.simulate, R.drawable.cmd_bloc, R.drawable.clp, R.drawable.aprob_clp, R.drawable.afis_clp, 
 			R.drawable.box_green_48, R.drawable.box_yellow_48, R.drawable.retur_marfa, R.drawable.status_retur_48, R.drawable.retur_comanda,
 			R.drawable.custody_48, R.drawable.data_icon,R.drawable.vanzari, R.drawable.neincasate, R.drawable.stoc_icon, R.drawable.dollar_icon, R.drawable.concurenta,
 			R.drawable.znecesar1_icon, R.drawable.status, R.drawable.target, R.drawable.money_gold_48, R.drawable.reduceri_icon,
